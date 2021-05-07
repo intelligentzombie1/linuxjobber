4f32ee1f441977300450448304d2958124b49a6c
intelligentzombie1 <kayodeoshodi400@gmail.com>
2021-05-07 13:01:44 -0400

    first commit

---
+++ kayodescrumy/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def get_grading_parameters(request):
+    return HttpResponse("Welcome to Django")
+
+# Create your views here.
