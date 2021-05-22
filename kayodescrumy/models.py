0de6a4dce276f26b9346d23811d0e34f5c3f800f
intelligentzombie1 <kayodeoshodi400@gmail.com>
2021-05-22 10:27:18 -0400

    nine again

--- kayodescrumy/models.py
+++ kayodescrumy/models.py
@@ -4,24 +4,24 @@ from django.contrib.auth.models import User
 
 
 class GoalStatus(models.Model):
-    status_name = models.TextField()
+    status_name = models.CharField(max_length=300)
 
 
 
 class ScrumyGoals(models.Model):
-    goal_name = models.TextField()
+    goal_name = models.CharField(max_length=300)
     goal_id = models.IntegerField()
-    created_by = models.TextField()
-    moved_by = models.TextField()
-    owner = models.TextField()
+    created_by = models.CharField(max_length=300)
+    moved_by = models.CharField(max_length=300)
+    owner = models.CharField(max_length=300)
     goal_status = models.ForeignKey(GoalStatus, on_delete=models.PROTECT)
     user = models.ForeignKey(User, related_name='user', on_delete=models.PROTECT)
 
 
 class ScrumyHistory(models.Model):
-    created_by = models.TextField()
-    moved_by = models.TextField()
-    moved_from = models.TextField()
-    moved_to = models.TextField()
-    time_of_action = models.TextField()
+    created_by = models.CharField(max_length=300)
+    moved_by = models.CharField(max_length=300)
+    moved_from = models.CharField(max_length=300)
+    moved_to = models.CharField(max_length=300)
+    time_of_action = models.TimeField()
     goal = models.ForeignKey(ScrumyGoals, on_delete=models.PROTECT)
