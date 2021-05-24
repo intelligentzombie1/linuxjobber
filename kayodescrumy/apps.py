194baeb2e694c5a2f432ae937bee7e926710999a
intelligentzombie1 <50877510+intelligentzombie1@users.noreply.github.com>
2021-05-24 22:41:08 +0100

    Delete kayodescrumy directory

--- kayodescrumy/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KayodescrumyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kayodescrumy'
--- kayodescrumy/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.get_grading_parameters, name='kayodescrumy/'),
-    path('movegoal/<int:goal_id>/', views.move_goal, name='move_goal')
-]
-
