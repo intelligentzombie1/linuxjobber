b055d66cd41302ff4e6435c151d510015221176b
intelligentzombie1 <kayodeoshodi400@gmail.com>
2021-05-24 12:32:28 -0400

    dead

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['54.160.183.46', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'kayodescrumy.apps.KayodescrumyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kayodescrumy',
 ]
 
 MIDDLEWARE = [
