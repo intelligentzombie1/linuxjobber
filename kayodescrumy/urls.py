a3660e992e67de1e5a9b535a5fa3b00b7c205bfa
intelligentzombie1 <kayodeoshodi400@gmail.com>
2021-05-28 14:05:46 -0400

    task 17

--- kayodescrumy/urls.py
+++ kayodescrumy/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from . import views
 
+app_name = "kayodescrumy"
+
 urlpatterns = [
-    path('', views.get_grading_parameters, name='kayodescrumy/'),
+    path('', views.get_grading_parameters, name='about/'),
     path('movegoal/<int:goal_id>/', views.move_goal, name='move_goal'),
     path('addgoal/', views.add_goal, name='addgoal'),
     path('home/', views.home, name='home'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('kayodescrumy/', include('kayodescrumy.urls')),
+    path('kayodescrumy/', include('kayodescrumy.urls', namespace='kayodescrumy')),
 ]
